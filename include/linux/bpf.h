6c2fb3d92ced4687b8430c10be66e4db33c35ed8
Roman Gushchin <guro@fb.com>
2018-07-13 12:41:10 -0700

    UPSTREAM: bpf: bpf_prog_array_alloc() should return a generic non-rcu pointer
    Currently the return type of the bpf_prog_array_alloc() is
    struct bpf_prog_array __rcu *, which is not quite correct.
    Obviously, the returned pointer is a generic pointer, which
    is valid for an indefinite amount of time and it's not shared
    with anyone else, so there is no sense in marking it as __rcu.
    
    This change eliminate the following sparse warnings:
    kernel/bpf/core.c:1544:31: warning: incorrect type in return expression (different address spaces)
    kernel/bpf/core.c:1544:31:    expected struct bpf_prog_array [noderef] <asn:4>*
    kernel/bpf/core.c:1544:31:    got void *
    kernel/bpf/core.c:1548:17: warning: incorrect type in return expression (different address spaces)
    kernel/bpf/core.c:1548:17:    expected struct bpf_prog_array [noderef] <asn:4>*
    kernel/bpf/core.c:1548:17:    got struct bpf_prog_array *<noident>
    kernel/bpf/core.c:1681:15: warning: incorrect type in assignment (different address spaces)
    kernel/bpf/core.c:1681:15:    expected struct bpf_prog_array *array
    kernel/bpf/core.c:1681:15:    got struct bpf_prog_array [noderef] <asn:4>*
    
    Bug: 254441685
    Fixes: 324bda9e6c5a ("bpf: multi program support for cgroup+bpf")
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit d29ab6e1fa21ebc2a8a771015dd9e0e5d4e28dc1)
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: I59d055c81b07be18766203f60a23005ebf340ad9
    Signed-off-by: shygosh <mossynine@gmail.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -276,7 +276,7 @@ struct bpf_prog_array {
 	struct bpf_prog *progs[0];
 };
 
-struct bpf_prog_array __rcu *bpf_prog_array_alloc(u32 prog_cnt, gfp_t flags);
+struct bpf_prog_array *bpf_prog_array_alloc(u32 prog_cnt, gfp_t flags);
 void bpf_prog_array_free(struct bpf_prog_array __rcu *progs);
 
 #define BPF_PROG_RUN_ARRAY(array, ctx, func)		\
