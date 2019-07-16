144363e9b7ba1dbde1f1b24b044a6b804aa41e29
Yafang Shao <laoar.shao@gmail.com>
2019-07-16 16:26:12 -0700

    mm/vmscan.c: calculate reclaimed slab caches in all reclaim paths
    There are six different reclaim paths by now:
    
     - kswapd reclaim path
     - node reclaim path
     - hibernate preallocate memory reclaim path
     - direct reclaim path
     - memcg reclaim path
     - memcg softlimit reclaim path
    
    The slab caches reclaimed in these paths are only calculated in the
    above three paths.
    
    There're some drawbacks if we don't calculate the reclaimed slab caches.
    
     - The sc->nr_reclaimed isn't correct if there're some slab caches
       relcaimed in this path.
    
     - The slab caches may be reclaimed thoroughly if there're lots of
       reclaimable slab caches and few page caches.
    
       Let's take an easy example for this case. If one memcg is full of
       slab caches and the limit of it is 512M, in other words there're
       approximately 512M slab caches in this memcg. Then the limit of the
       memcg is reached and the memcg reclaim begins, and then in this memcg
       reclaim path it will continuesly reclaim the slab caches until the
       sc->priority drops to 0. After this reclaim stops, you will find
       there're few slab caches left, which is less than 20M in my test
       case. While after this patch applied the number is greater than 300M
       and the sc->priority only drops to 3.
    
    Link: http://lkml.kernel.org/r/1561112086-6169-3-git-send-email-laoar.shao@gmail.com
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Kirill Tkhai <ktkhai@virtuozzo.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3280,6 +3280,7 @@ unsigned long try_to_free_pages(struct zonelist *zonelist, int order,
 	if (throttle_direct_reclaim(sc.gfp_mask, zonelist, nodemask))
 		return 1;
 
+	current->reclaim_state = &sc.reclaim_state;
 	trace_mm_vmscan_direct_reclaim_begin(order,
 				sc.may_writepage,
 				sc.gfp_mask,
@@ -3288,6 +3289,7 @@ unsigned long try_to_free_pages(struct zonelist *zonelist, int order,
 	nr_reclaimed = do_try_to_free_pages(zonelist, &sc);
 
 	trace_mm_vmscan_direct_reclaim_end(nr_reclaimed);
+	current->reclaim_state = NULL;
 
 	return nr_reclaimed;
 }
@@ -3309,6 +3311,7 @@ unsigned long mem_cgroup_shrink_node(struct mem_cgroup *memcg,
 	};
 	unsigned long lru_pages;
 
+	current->reclaim_state = &sc.reclaim_state;
 	sc.gfp_mask = (gfp_mask & GFP_RECLAIM_MASK) |
 			(GFP_HIGHUSER_MOVABLE & ~GFP_RECLAIM_MASK);
 
@@ -3328,7 +3331,9 @@ unsigned long mem_cgroup_shrink_node(struct mem_cgroup *memcg,
 
 	trace_mm_vmscan_memcg_softlimit_reclaim_end(sc.nr_reclaimed);
 
+	current->reclaim_state = NULL;
 	*nr_scanned = sc.nr_scanned;
+
 	return sc.nr_reclaimed;
 }
 
@@ -3354,6 +3359,7 @@ unsigned long try_to_free_mem_cgroup_pages(struct mem_cgroup *memcg,
 		.may_swap = may_swap,
 	};
 
+	current->reclaim_state = &sc.reclaim_state;
 	/*
 	 * Unlike direct reclaim via alloc_pages(), memcg's reclaim doesn't
 	 * take care of from where we get pages. So the node where we start the
@@ -3377,6 +3383,7 @@ unsigned long try_to_free_mem_cgroup_pages(struct mem_cgroup *memcg,
 	psi_memstall_leave(&pflags);
 
 	trace_mm_vmscan_memcg_reclaim_end(nr_reclaimed);
+	current->reclaim_state = NULL;
 
 	return nr_reclaimed;
 }
