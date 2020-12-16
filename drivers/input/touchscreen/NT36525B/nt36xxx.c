2cfebda1622945d777848dc48abfe93b0b8b4577
Yaroslav Furman <yaro330@gmail.com>
2020-12-16 12:37:09 +0300

    nt36xxx: switch to device initcall
    Device boots and dt2w switch works.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: azrim <mirzaspc@gmail.com>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- drivers/input/touchscreen/NT36525B/nt36xxx.c
+++ drivers/input/touchscreen/NT36525B/nt36xxx.c
@@ -3027,8 +3027,7 @@ static void __exit nvt_driver_exit(void)
 #endif /* #if NVT_TOUCH_ESD_PROTECT */
 }
 
-//late_initcall(nvt_driver_init);
-module_init(nvt_driver_init);
+device_initcall(nvt_driver_init);
 module_exit(nvt_driver_exit);
 
 MODULE_DESCRIPTION("Novatek Touchscreen Driver");
