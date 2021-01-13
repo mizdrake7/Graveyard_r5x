6589c89dbc51df72ab8d758689352f0aa1322c4e
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-01-13 17:25:24 +0100

    fs: sdcardfs: Fix misleading indentation warning.
    fixes:
    
    ../fs/sdcardfs/dev.c:145:2: warning: misleading indentation; statement is not part of the previous 'for' [-Wmisleading-indentation]
            return 0;
            ^
    ../fs/sdcardfs/dev.c:142:5: note: previous statement is here
        for(i--; i > 0; i--)
        ^
    ../fs/sdcardfs/dev.c:254:2: warning: misleading indentation; statement is not part of the previous 'for' [-Wmisleading-indentation]
            kmem_cache_free(sdcardfs_path_cachep, buf);
            ^
    ../fs/sdcardfs/dev.c:251:5: note: previous statement is here
        for(i--; i > 0; i--)
        ^
    ../fs/sdcardfs/dev.c:308:2: warning: misleading indentation; statement is not part of the previous 'for' [-Wmisleading-indentation]
            kmem_cache_free(sdcardfs_path_cachep, buf);
            ^
    ../fs/sdcardfs/dev.c:305:5: note: previous statement is here
        for(i--; i > 0; i--)
        ^
    
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: mcdofrenchfreis <xyzevan@androidist.net>

--- fs/sdcardfs/dev.c
+++ fs/sdcardfs/dev.c
@@ -139,8 +139,8 @@ static int sdcardfs_rename_uevent(char *old_path, char *new_path, unsigned int m
 	}
 
 free_memory:
-    for(i--; i > 0; i--)
-        kmem_cache_free(sdcardfs_path_cachep, denied_param[i]);
+	for(i--; i > 0; i--)
+		kmem_cache_free(sdcardfs_path_cachep, denied_param[i]);
 
 	return 0;
 }
@@ -249,8 +249,8 @@ int sdcardfs_allunlink_uevent(struct dentry *dentry)
 	}
 
 free_memory:
-    for(i--; i > 0; i--)
-        kmem_cache_free(sdcardfs_path_cachep, denied_param[i]);
+	for(i--; i > 0; i--)
+		kmem_cache_free(sdcardfs_path_cachep, denied_param[i]);
 
 	kmem_cache_free(sdcardfs_path_cachep, buf);
 
@@ -303,8 +303,8 @@ int sdcardfs_unlink_uevent(struct dentry *dentry, unsigned int mask)
 	}
 
 free_memory:
-    for(i--; i > 0; i--)
-        kmem_cache_free(sdcardfs_path_cachep, denied_param[i]);
+	for(i--; i > 0; i--)
+		kmem_cache_free(sdcardfs_path_cachep, denied_param[i]);
 
 	kmem_cache_free(sdcardfs_path_cachep, buf);
 
