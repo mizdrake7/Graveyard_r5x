f5db1ccd95b365be317842a812803dde1bf90add
Alexander Lobakin <alobakin@pm.me>
2021-02-16 08:55:25 +0000

    vmlinux.lds.h: catch more UBSAN symbols into .data
    LKP triggered lots of LD orphan warnings [0]:
    
    mipsel-linux-ld: warning: orphan section `.data.$Lubsan_data299' from
    `init/do_mounts_rd.o' being placed in section `.data.$Lubsan_data299'
    mipsel-linux-ld: warning: orphan section `.data.$Lubsan_data183' from
    `init/do_mounts_rd.o' being placed in section `.data.$Lubsan_data183'
    mipsel-linux-ld: warning: orphan section `.data.$Lubsan_type3' from
    `init/do_mounts_rd.o' being placed in section `.data.$Lubsan_type3'
    mipsel-linux-ld: warning: orphan section `.data.$Lubsan_type2' from
    `init/do_mounts_rd.o' being placed in section `.data.$Lubsan_type2'
    mipsel-linux-ld: warning: orphan section `.data.$Lubsan_type0' from
    `init/do_mounts_rd.o' being placed in section `.data.$Lubsan_type0'
    
    [...]
    
    Seems like "unnamed data" isn't the only type of symbols that UBSAN
    instrumentation can emit.
    Catch these into .data with the wildcard as well.
    
    [0] https://lore.kernel.org/linux-mm/202102160741.k57GCNSR-lkp@intel.com
    
    Fixes: f41b233de0ae ("vmlinux.lds.h: catch UBSAN's "unnamed data" into data")
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Alexander Lobakin <alobakin@pm.me>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: nantaaa <anantah738@gmail.com>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -71,7 +71,7 @@
 #ifdef CONFIG_LD_DEAD_CODE_DATA_ELIMINATION
 #define TEXT_MAIN .text .text.[0-9a-zA-Z_]*
 #define TEXT_CFI_MAIN .text.cfi .text.[0-9a-zA-Z_]*.cfi
-#define DATA_MAIN .data .data.[0-9a-zA-Z_]* .data..compoundliteral* .data..L_MergedGlobals* .data.$__unnamed_*
+#define DATA_MAIN .data .data.[0-9a-zA-Z_]* .data..compoundliteral* .data..L_MergedGlobals* .data.$__unnamed_* .data.$Lubsan_*
 #define SDATA_MAIN .sdata .sdata.[0-9a-zA-Z_]* .sdata..compoundliteral* .sdata..L_MergedGlobals*
 #define RODATA_MAIN .rodata .rodata.[0-9a-zA-Z_]* .rodata..compoundliteral* .rodata..L_MergedGlobals*
 #define BSS_MAIN .bss .bss.[0-9a-zA-Z_]* .bss..compoundliteral* .bss..L_MergedGlobals*
