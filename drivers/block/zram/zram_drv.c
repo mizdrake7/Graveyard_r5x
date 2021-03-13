434d83bbd974f772b3b78ff14fa4db8c89b3aa99
Minchan Kim <minchan@kernel.org>
2021-03-12 21:08:38 -0800

    UPSTREAM: zram: fix return value on writeback_store
    writeback_store's return value is overwritten by submit_bio_wait's return
    value.  Thus, writeback_store will return zero since there was no IO
    error.  In the end, write syscall from userspace will see the zero as
    return value, which could make the process stall to keep trying the write
    until it will succeed.
    
    Link: https://lkml.kernel.org/r/20210312173949.2197662-1-minchan@kernel.org
    Fixes: 3b82a051c101("drivers/block/zram/zram_drv.c: fix error return codes not being returned in writeback_store")
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Cc: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Colin Ian King <colin.king@canonical.com>
    Cc: John Dias <joaodias@google.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 57e0076e6575a7b7cef620a0bd2ee2549ef77818)
    
    Bug: 182850185
    Signed-off-by: Amos Bianchi <amosbianchi@google.com>
    Change-Id: I7cbef91b62584826192b7ebdb434675a549a9801
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -642,7 +642,7 @@ static ssize_t writeback_store(struct device *dev,
 	struct bio_vec bio_vec;
 	struct page *page;
 	ssize_t ret;
-	int mode;
+	int mode, err;
 	unsigned long blk_idx = 0;
 
 	if (sysfs_streq(buf, "idle"))
