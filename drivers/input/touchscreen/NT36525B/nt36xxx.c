a58671fb8999c0ecb0d9fa71782c551147a9611a
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2021-03-30 12:44:14 +0700

    drivers: input: touchscreen: Use KEY_WAKEUP for oppo touchpanel gesture
    Co-authored-by: karthick111 <karthickrko61@gmail.com>

--- drivers/input/touchscreen/NT36525B/nt36xxx.c
+++ drivers/input/touchscreen/NT36525B/nt36xxx.c
@@ -1266,7 +1266,7 @@ void nvt_ts_wakeup_gesture_report(uint8_t gesture_id, uint8_t *data, struct gest
 			gesture->Point_4th.x, gesture->Point_4th.y);
 
 	if (gesture->gesture_type != UnkownGesture) {
-		keycode = KEY_F4;
+		keycode = KEY_WAKEUP;
 
 		input_report_key(ts->input_dev, keycode, 1);
 		input_sync(ts->input_dev);
@@ -2575,7 +2575,7 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 #if WAKEUP_GESTURE
 	ts->gesture_enable = 0;
 	memset(&ts->gesture, 0, sizeof(struct gesture_info));
-	input_set_capability(ts->input_dev, EV_KEY, KEY_F4);
+	input_set_capability(ts->input_dev, EV_KEY, KEY_WAKEUP);
 #ifdef CONFIG_ODM_WT_EDIT
 //Bin.Su@ODM_WT.BSP.TP.FUNCTION.2018/11/20,solve novatek build error
 	//device_init_wakeup(&ts->input_dev->dev, true);
