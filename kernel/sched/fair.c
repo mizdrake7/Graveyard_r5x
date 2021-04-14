20f4d628d085c2f5efa72cf68a99ec4eb4bd9ac6
Diep Quynh <remilia.1505@gmail.com>
2021-04-14 17:53:58 +0700

    sched: fair: Modify capacity margins for sm6125
    This is tuned to match energy model characteristics and scheduler
    efficiency enhancements
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -194,7 +194,8 @@ unsigned int capacity_margin				= 1280;
 
 /* Migration margins */
 unsigned int sysctl_sched_capacity_margin_up[MAX_MARGIN_LEVELS] = {
-			[0 ... MAX_MARGIN_LEVELS-1] = 1078}; /* ~5% margin */
+	[0 ... MAX_MARGIN_LEVELS - 1] = 1462
+}; /* ~30% margin */
 unsigned int sysctl_sched_capacity_margin_down[MAX_MARGIN_LEVELS] = {
 	[0 ... MAX_MARGIN_LEVELS-1] = 1205}; /* ~15% margin */
 unsigned int sysctl_sched_capacity_margin_up_boosted[MAX_MARGIN_LEVELS] = {
@@ -203,7 +204,8 @@ unsigned int sysctl_sched_capacity_margin_down_boosted[MAX_MARGIN_LEVELS] = {
 	[0 ... MAX_MARGIN_LEVELS-1] = 1280}; /* ~20% margin */
 
 unsigned int sched_capacity_margin_up[NR_CPUS] = {
-	[0 ... NR_CPUS-1] = 1078}; /* ~5% margin */
+	1462, 1462, 1462, 1462, 1280, 1280, 1280, 1280
+}; /* ~30% margin for small, 20% for big */
 unsigned int sched_capacity_margin_down[NR_CPUS] = {
 	[0 ... NR_CPUS-1] = 1205}; /* ~15% margin */
 unsigned int sched_capacity_margin_up_boosted[NR_CPUS] = {
