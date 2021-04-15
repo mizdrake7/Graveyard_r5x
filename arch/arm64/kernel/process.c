8ebaec0f1200c31e069a974aab125ee6fb383f29
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2021-04-15 18:28:53 +0000

    arm64: kernel: Import Realme Narzo 20A Q-OSS changes

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -238,6 +238,9 @@ static void show_extra_register_data(struct pt_regs *regs, int nbytes)
 	set_fs(fs);
 }
 
+#ifdef VENDOR_EDIT //yixue.ge@bsp.drv add for dump cpu contex for minidump
+extern void dumpcpuregs(struct pt_regs *pt_regs);
+#endif
 void __show_regs(struct pt_regs *regs)
 {
 	int i, top_reg;
@@ -253,6 +256,9 @@ void __show_regs(struct pt_regs *regs)
 		top_reg = 29;
 	}
 
+#ifdef VENDOR_EDIT //yixue.ge@bsp.drv add for dump cpu contex for minidump
+	dumpcpuregs(regs);
+#endif
 	show_regs_print_info(KERN_DEFAULT);
 	print_symbol("pc : %s\n", regs->pc);
 	print_symbol("lr : %s\n", lr);
