396b1010cdcec529506132bf462d9e7dd0817320
Zhen Lei <thunder.leizhen@huawei.com>
2021-06-30 18:55:55 -0700

    lib/decompressors: fix spelling mistakes
    Fix some spelling mistakes in comments:
    sentinal ==> sentinel
    compresed ==> compressed
    dependeny ==> dependency
    immediatelly ==> immediately
    dervied ==> derived
    splitted ==> split
    nore ==> not
    independed ==> independent
    asumed ==> assumed
    
    Link: https://lkml.kernel.org/r/20210604085656.12257-1-thunder.leizhen@huawei.com
    Signed-off-by: Zhen Lei <thunder.leizhen@huawei.com>
    Cc: Jiri Kosina <jkosina@suse.cz>
    Cc: Thomas Bogendoerfer <tsbogend@alpha.franken.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/decompress_unzstd.c
+++ lib/decompress_unzstd.c
@@ -16,7 +16,8 @@
  * uncompressible. Thus, we must look for worst-case expansion when the
  * compressor is encoding uncompressible data.
  *
- * The structure of the .zst file in case of a compresed kernel is as follows.
+
+ * The structure of the .zst file in case of a compressed kernel is as follows.
 
  * Maximum sizes (as bytes) of the fields are in parenthesis.
  *
@@ -57,8 +58,7 @@
 /*
  * Preboot environments #include "path/to/decompress_unzstd.c".
  * All of the source files we depend on must be #included.
-
- * zstd's only source dependeny is xxhash, which has no source
+ * zstd's only source dependency is xxhash, which has no source
 
  * dependencies.
  *
