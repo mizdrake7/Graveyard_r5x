d25dda35ab71e1de0d34647b7bb00ce604fdedc5
Adam W. Willis <return.of.octobot@gmail.com>
2021-07-17 20:47:03 +0700

    lowmemorykiller: Disable debug logging
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: WisnuArdhi27 <wisnuardhi07@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -81,7 +81,7 @@ static void lowmemorykiller_work_func(struct work_struct *work);
 static int enable_lmk = 1;
 module_param_named(enable_lmk, enable_lmk, int, 0644);
 
-static u32 lowmem_debug_level = 1;
+static u32 lowmem_debug_level = 0;
 static short lowmem_adj[6] = {
 	0,
 	1,
