c8d0231e6b75203888e7c5a7d0eb4643a6f79972
Sultan Alsawaf <sultan@kerneltoast.com>
2021-07-18 11:19:55 +0700

    drm/msm/sde: Fix unchecked must_check DRM helper return values
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: WisnuArdhi27 <wisnuardhi07@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_kms.c
+++ drivers/gpu/drm/msm/sde/sde_kms.c
@@ -2767,7 +2767,11 @@ retry:
 	crtc_state->active = true;
 	drm_atomic_set_crtc_for_connector(conn_state, enc->crtc);
 
-	drm_atomic_commit(state);
+	ret = drm_atomic_commit(state);
+	if (ret) {
+		DRM_ERROR("error %d committing state\n", ret);
+		goto end;
+	}
 end:
 	if (state)
 		drm_atomic_state_put(state);
