0c97b5cf2c082011b7dbc46202ae534cb6b4f2a4
henrysg29 <18201042@ittelkom-pwt.ac.id>
2021-08-10 20:17:25 +0000

    drivers: devfreq: removing partner gpu profile

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -653,9 +653,6 @@ static int tz_handler(struct devfreq *devfreq, unsigned int event, void *data)
 		cancel_delayed_work_sync(&unboost_work);
 		tz_devfreq_g = NULL;
 		/* Queue the stop work before the TZ is stopped */
-		//if (partner_gpu_profile && partner_gpu_profile->bus_devfreq)
-		//	queue_work(workqueue,
-		//		&gpu_profile->partner_stop_event_ws);
 		spin_lock(&suspend_lock);
 		suspend_start = 0;
 		spin_unlock(&suspend_lock);
