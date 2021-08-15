fa5b1d90891cdd97ad62a8f81845f6da1df321a7
henrysg29 <18201042@ittelkom-pwt.ac.id>
2021-08-15 10:55:54 +0000

    fs: exec: fixing undeclare line from bde20aede0bfa79751893d8b44643a863ee708ce

--- fs/exec.c
+++ fs/exec.c
@@ -1831,7 +1831,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 		if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN)))
 			zygote32_pid = current->pid;
 		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
-			zygote64_sig = current->signal;
+			zygote64_pid = current->signal;
 		else if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
