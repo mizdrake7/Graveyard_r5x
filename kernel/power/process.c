42254ee5a0cc8c15ea4962abe28d8672913cf9aa
Sultan Alsawaf <sultan@kerneltoast.com>
2022-05-04 09:28:34 -0700

    PM / freezer: Abort suspend when there's a wakeup while freezing
    Although try_to_freeze_tasks() stops when there's a wakeup, it doesn't
    return an error when it successfully freezes everything it wants to freeze.
    As a result, the suspend attempt can continue even after a wakeup is
    issued. Although the wakeup will be eventually caught later in the suspend
    process, kicking the can down the road is suboptimal; when there's a wakeup
    detected, suspend should be immediately aborted by returning an error
    instead. Make try_to_freeze_tasks() do just that, and also move the wakeup
    check above the `todo` check so that we don't miss a wakeup from a process
    that successfully froze.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -69,9 +69,6 @@ static int try_to_freeze_tasks(bool user_only)
 			todo += wq_busy;
 		}
 
-		if (!todo || time_after(jiffies, end_time))
-			break;
-
 		if (pm_wakeup_pending()) {
 #ifdef CONFIG_PM_SLEEP
 			pm_get_active_wakeup_sources(suspend_abort,
@@ -81,6 +78,9 @@ static int try_to_freeze_tasks(bool user_only)
 			wakeup = true;
 			break;
 		}
+
+		if (!todo || time_after(jiffies, end_time))
+			break;
 
 		/*
 		 * We need to retry, but first give the freezing tasks some
@@ -122,7 +122,7 @@ static int try_to_freeze_tasks(bool user_only)
 			elapsed_msecs % 1000);
 	}
 
-	return todo ? -EBUSY : 0;
+	return todo || wakeup ? -EBUSY : 0;
 }
 
 /**
