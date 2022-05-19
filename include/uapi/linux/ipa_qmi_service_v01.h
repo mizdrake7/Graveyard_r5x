f95b930ac41222a5ad0b820caf244b927a277e9b
mARk <r3066.funtab@gmail.com>
2022-05-19 16:04:19 +0200

    msm: ipa_v3: Fix filter enum mismatch and ip_type conversion
    dirty patch
    
    There is a mismatch between the signed inner structure that causes this encoding problem
    with clang 15 llvm-project commit:
    [PATCH] Enum conversion warning when one signed and other unsigned
    882915df61e33f3a2b7f58e52f572717e1c11499
    
    ../drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c:505:41: error: implicit conversion from enumeration type 'enum ipa_ip_type_enum_v01' to different enumeration type 'enum ipa_ip_type' [-Werror,-Wenum-conversion]
            q6_ul_flt_rule_ptr->ip = flt_spec_ptr->ip_type;
                                   ~ ~~~~~~~~~~~~~~^~~~~~~
    ../drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c:506:45: error: implicit conversion from enumeration type 'enum ipa_filter_action_enum_v01' to different enumeration type 'enum ipa_flt_action' [-Werror,-Wenum-conversion]
            q6_ul_flt_rule_ptr->action = flt_spec_ptr->filter_action;
                                       ~ ~~~~~~~~~~~~~~^~~~~~~~~~~~~
    
    Change-Id: I2d1ac1f023fff2f50b8c39943cad16951f855ed1
    Signed-off-by: mARk <r3066.funtab@gmail.com>

--- include/uapi/linux/ipa_qmi_service_v01.h
+++ include/uapi/linux/ipa_qmi_service_v01.h
@@ -942,7 +942,7 @@ struct ipa_filter_spec_ex_type_v01 {
 };  /* Type */
 
 struct ipa_filter_spec_ex2_type_v01 {
-	enum ipa_ip_type_enum_v01 ip_type;
+	uint16_t ip_type;
 	/*	This field identifies the IP type for which this rule is
 	 *	applicable. The driver needs to identify the filter table
 	 *	(V6 or V4) and this field is essential for that
@@ -953,7 +953,7 @@ struct ipa_filter_spec_ex2_type_v01 {
 	 *	are the ones that are matched against fields in the packet.
 	 */
 
-	enum ipa_filter_action_enum_v01 filter_action;
+	uint16_t filter_action;
 	/*	This field specifies the action to be taken when a filter match
 	 *	occurs. The remote side should install this information into the
 	 *	hardware along with the filter equations.
