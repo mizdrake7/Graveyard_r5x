338809771b4d9e6147841ab3ac2f0811c7661129
lllgts <wuer031@126.com>
2022-06-09 05:53:38 +0000

    sched/features: Tweak for performance
    Signed-off-by: lllgts <wuer031@126.com>
    Signed-off-by: negrroo <mohammedaelnaggar1@gmail.com>
    Signed-off-by: Muhammed Ali Simsek <malisimsek17@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -124,7 +124,7 @@ SCHED_FEAT(ENERGY_AWARE, false)
  */
 SCHED_FEAT(EAS_PREFER_IDLE, false)
 SCHED_FEAT(FIND_BEST_TARGET, true)
-SCHED_FEAT(FBT_STRICT_ORDER, false)
+SCHED_FEAT(FBT_STRICT_ORDER, true)
 
 /*
  * Apply schedtune boost hold to tasks of all sched classes.
