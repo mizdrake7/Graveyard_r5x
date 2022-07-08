bc0b32e0dcb9f051f513b01e42b634c407c9ada7
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2022-07-08 13:01:43 +0700

    sched/tune: Disable boost & prefer_idle when profiles is on level 1
    * Inspired from commit https://github.com/ghostrider-reborn/android_kernel_xiaomi_ginkgo/commit/db2096821c5ace1def3ac0f56b2e7543b2b3153e
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -21,6 +21,10 @@
 bool schedtune_initialized = false;
 extern struct reciprocal_value schedtune_spc_rdiv;
 
+#ifdef CONFIG_KPROFILES
+extern int kp_active_mode(void);
+#endif
+
 /* We hold schedtune boost in effect for at least this long */
 #define SCHEDTUNE_BOOST_HOLD_NS 50000000ULL
 
@@ -695,6 +699,11 @@ int schedtune_task_boost(struct task_struct *p)
 	if (unlikely(!schedtune_initialized) || is_battery_saver_on())
 		return 0;
 
+#ifdef CONFIG_KPROFILES
+	if (kp_active_mode() == 1)
+		return 0;
+#endif
+
 	/* Get task boost value */
 	rcu_read_lock();
 	st = task_schedtune(p);
@@ -738,6 +747,11 @@ int schedtune_prefer_idle(struct task_struct *p)
 	if (unlikely(!schedtune_initialized) || is_battery_saver_on())
 		return 0;
 
+#ifdef CONFIG_KPROFILES
+	if (kp_active_mode() == 1)
+		return 0;
+#endif
+
 	/* Get prefer_idle value */
 	rcu_read_lock();
 	st = task_schedtune(p);
@@ -754,6 +768,11 @@ prefer_idle_read(struct cgroup_subsys_state *css, struct cftype *cft)
 
 	if (is_battery_saver_on())
 		return 0;
+
+#ifdef CONFIG_KPROFILES
+	if (kp_active_mode() == 1)
+		return 0;
+#endif
 
 	return st->prefer_idle;
 }
@@ -775,6 +794,11 @@ boost_read(struct cgroup_subsys_state *css, struct cftype *cft)
 
 	if (is_battery_saver_on())
 		return 0;
+
+#ifdef CONFIG_KPROFILES
+	if (kp_active_mode() == 1)
+		return 0;
+#endif
 
 	return st->boost;
 }
