e45406e9429c1a3e190e7b0910a92567fbc1d8ac
Juhyung Park <qkrwngud825@gmail.com>
2022-07-20 18:36:30 +0900

    power: oppo: remove unused enable_charger_log
    Nothing is CHG_LOG_FULL, remove enable_charger_log
    
    Change-Id: If4bcf99305b81c6bd537c41fa40dc2dfedb97a1f
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    [adapt for oppo charger]
    Signed-off-by: nantaaa <anantah738@gmail.com>

--- drivers/power/oppo/oppo_vooc.c
+++ drivers/power/oppo/oppo_vooc.c
@@ -35,13 +35,7 @@
 #define VOOC_NOTIFY_ADAPTER_MODEL_FACTORY            0x5e
 
 extern int charger_abnormal_log;
-extern int enable_charger_log;
-#define vooc_xlog_printk(num, fmt, ...) \
-        do { \
-                if (enable_charger_log >= (int)num) { \
-                        printk(KERN_NOTICE pr_fmt("[OPPO_CHG][%s]"fmt), __func__, ##__VA_ARGS__);\
-        } \
-} while (0)
+#define vooc_xlog_printk(num, ...) chg_debug(__VA_ARGS__)
 
 
 static struct oppo_vooc_chip *g_vooc_chip = NULL;
