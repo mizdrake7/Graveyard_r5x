5ef85a6b306dede30ee6ac0effac751cee5353b3
RyuujiX <saputradenny712@gmail.com>
2023-02-27 06:02:09 +0700

    fs: Use KernelSU Second Way Implementation
    - Seems KProbes is broken.
    - Based on https://kernelsu.org/guide/how-to-integrate-for-non-gki.html.
    
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>
    Signed-off-by: danii <daniviandra57@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1717,6 +1717,9 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1727,6 +1730,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
