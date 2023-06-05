dd4762db688799ad99b76b5a3d9b9395035e9ffb
MAdMiZ <mizdrake7@gmail.com>
2023-06-05 05:30:45 +0000

    Revert "sched: Use bitmask for sched_busy_hysteresis_enable_cpus tunable"
    This reverts commit ab50f34b3d7955eae98ba814d6b207ef4919b5ef.

--- kernel/sched/sched_avg.c
+++ kernel/sched/sched_avg.c
@@ -32,7 +32,9 @@ static DEFINE_PER_CPU(u64, nr_max);
 static DEFINE_PER_CPU(spinlock_t, nr_lock) = __SPIN_LOCK_UNLOCKED(nr_lock);
 static s64 last_get_time;
 
-unsigned int sysctl_sched_busy_hysteresis_enable_cpus;
+static struct cpumask sched_busy_hysteresis_cpumask;
+unsigned long *sched_busy_hysteresis_cpubits =
+				cpumask_bits(&sched_busy_hysteresis_cpumask);
 static DEFINE_PER_CPU(atomic64_t, last_busy_time) = ATOMIC64_INIT(0);
 
 #define NR_THRESHOLD_PCT		15
@@ -110,7 +112,7 @@ static inline void update_last_busy_time(int cpu, bool dequeue,
 {
 	bool nr_run_trigger = false, load_trigger = false;
 
-	if (!(BIT(cpu) & sysctl_sched_busy_hysteresis_enable_cpus))
+	if (!cpumask_test_cpu(cpu, &sched_busy_hysteresis_cpumask))
 		return;
 
 	if (prev_nr_run >= BUSY_NR_RUN && per_cpu(nr, cpu) < BUSY_NR_RUN)
--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -569,12 +569,10 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &max_sched_granularity_ns,
 	},
 		.procname	= "sched_busy_hysteresis_enable_cpus",
-		.data		= &sysctl_sched_busy_hysteresis_enable_cpus,
-		.maxlen		= sizeof(unsigned int),
+		.data		= &sched_busy_hysteresis_cpubits,
+		.maxlen		= NR_CPUS,
 		.mode		= 0644,
-		.proc_handler	= proc_douintvec_minmax,
-		.extra1		= &zero,
-		.extra2		= &two_hundred_fifty_five,
+		.proc_handler	= proc_do_large_bitmap,
 	},
 #endif
 	{
