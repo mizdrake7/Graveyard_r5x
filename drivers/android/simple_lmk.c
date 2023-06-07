e83f6dd9514f9342b86013969e1e923e73f983cf
Cyber Knight <cyberknight755@gmail.com>
2023-06-07 14:56:59 +0530

    simple_lmk: Dynamically set minfree and timeout values
    - These values are adequate for 3GB and 4GB ram variants.
    - Also grab conditional check fix from [1].
    
    [1]: Dark-Matter7232/CosmicFresh-Hanoip@31428c7
    
    [Thanks to eun for 4GB RAM variant slmk values]
    
    Change-Id: I41fe06622d425709f35df12ab730ed2f656081d3
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -341,12 +341,23 @@ static int simple_lmk_init_set(const char *val, const struct kernel_param *kp)
 {
 	static atomic_t init_done = ATOMIC_INIT(0);
 	struct task_struct *thread;
+	struct sysinfo i;
 
 	if (!atomic_cmpxchg(&init_done, 0, 1)) {
 		thread = kthread_run(simple_lmk_reclaim_thread, NULL,
 				     "simple_lmkd");
 		BUG_ON(IS_ERR(thread));
 		BUG_ON(vmpressure_notifier_register(&vmpressure_notif));
+	}
+
+	si_meminfo(&i);
+	if (i.totalram << (PAGE_SHIFT-10) > 3072ull * 1024) {
+	  // from - phone-xhdpi-4096-dalvik-heap.mk
+	  slmk_minfree = 64;
+	  slmk_timeout = 172;
+	} else {
+	  slmk_minfree = 64;
+	  slmk_timeout = 250;
 	}
 
 	return 0;
