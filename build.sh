9d07a725d50a26e3643b2468ad5d9976af9c61cf
MAdMiZ <mizdrake7@gmail.com>
2023-07-02 21:46:48 +0000

    build.sh: Add output ZIP file size in megabytes to our compilation script
    * This commit modifies the compilation script to calculate and display the size of the generated ZIP file in megabytes. It enhances the existing functionality by dividing the file size by 1,048,576 (1024^2) and echoing the result. The updated script provides more comprehensive information about the size of the ZIP file during the compilation process.
    
    * The modified script ensures better visibility and understanding of the resulting file size for developers and users. This commit improves the overall user experience when working with the compilation script.
    
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- build.sh
+++ build.sh
@@ -57,26 +57,33 @@ echo -e "\nStarting compilation...\n"
 make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img 2>&1 | tee error.log
 
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
-echo -e "\nKernel compiled succesfully! Zipping up...\n"
-if [ -d "$AK3_DIR" ]; then
-cp -r $AK3_DIR AnyKernel3
-elif ! git clone -q https://github.com/mizdrake7/AnyKernel3; then
-echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
-exit 1
-fi
-cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
-cp out/arch/arm64/boot/dtbo.img AnyKernel3
-rm -f *zip
-cd AnyKernel3
-git checkout master &> /dev/null
-zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
-cd ..
-rm -rf AnyKernel3
-rm -rf out/arch/arm64/boot
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-echo "Zip: $ZIPNAME"
-curl --upload-file $ZIPNAME https://temp.sh/$ZIPNAME; echo
+  echo -e "\nKernel compiled successfully! Zipping up...\n"
+  if [ -d "$AK3_DIR" ]; then
+    cp -r "$AK3_DIR" AnyKernel3
+  elif ! git clone -q https://github.com/mizdrake7/AnyKernel3; then
+    echo -e "\nAnyKernel3 repo not found locally, and cloning failed! Aborting..."
+    exit 1
+  fi
+  cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+  cp out/arch/arm64/boot/dtbo.img AnyKernel3
+  rm -f *zip
+  cd AnyKernel3
+  git checkout master &> /dev/null
+  zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+  cd ..
+  rm -rf AnyKernel3
+  rm -rf out/arch/arm64/boot
+  echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!"
+  echo "Zip: $ZIPNAME"
+  # Get the size of the ZIP file in megabytes
+  ZIP_SIZE=$(stat -c%s "$ZIPNAME")
+  ZIP_SIZE_MB=$(awk "BEGIN {print $ZIP_SIZE/1048576}")
+  echo "Zip Size: $ZIP_SIZE_MB MB"
+  # Upload the ZIP file
+  echo "Uploading the ZIP file..."
+  curl --upload-file "$ZIPNAME" "https://temp.sh/$ZIPNAME"
+  echo
 else
-echo -e "\nCompilation failed!"
-exit 1
+  echo -e "\nCompilation failed!"
+  exit 1
 fi
