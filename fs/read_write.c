254553a3bd862fe1a27a93a30aedea763f9be699
Akari <akariondev@gmail.com>
2023-10-21 16:03:12 -0300

    fs: update ksu hooks
    Signed-off-by: Akari <akariondev@gmail.com>

--- fs/read_write.c
+++ fs/read_write.c
@@ -431,7 +431,6 @@ EXPORT_SYMBOL(kernel_read);
 
 extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
 			size_t *count_ptr, loff_t **pos);
-
 ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
