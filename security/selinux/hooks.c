65785b926723f92c91c44d3c4e0e4c2c58229bd1
Ruri <barryofc11@gmail.com>
2023-10-21 01:48:58 -0300

    security: selinux: allow init exec ksud under nosuid security/selinux/hooks.c: Enhance NNP and nosuid checks
    In the security/selinux/hooks.c file, this commit improves the NNP (No New Privileges) and nosuid checks for better security. The changes include:
    
    - Introducing a static variable, ksu_sid, to store a specific security identifier.
    - Checking for NNP and nosuid conditions based on the `unsafe` flag and mount flags.
    - Retrieving and comparing security context information to determine whether the old security context is "u:r:init:s0" and the new security context is equal to ksu_sid.
    - Ensuring that if both NNP and nosuid conditions are not met, the function returns 0.
    
    These changes aim to enhance security and access control within the SELinux subsystem.
    
    Signed-off-by: Akari <moenyanuwu@gmail.com>
    Co-authored-by: weishu <twsxtd@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2362,9 +2362,10 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 	static u32 ksu_sid;
 	char *secdata;
 	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
-	int nosuid = (bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID);
-	int rc,error;
-	u32 seclen,av;
+	int nosuid = !mnt_may_suid(bprm->file->f_path.mnt);
+	int rc, error;
+	u32 av;
+	u32 seclen;
 
 	if (!nnp && !nosuid)
 		return 0; /* neither NNP nor nosuid */
@@ -2372,11 +2373,12 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 	if (new_tsec->sid == old_tsec->sid)
 		return 0; /* No change in credentials */
 
-
 	if(!ksu_sid){
 		security_secctx_to_secid("u:r:su:s0", strlen("u:r:su:s0"), &ksu_sid);
 	}
+
 	error = security_secid_to_secctx(old_tsec->sid, &secdata, &seclen);
+
 	if (!error) {
 		rc = strcmp("u:r:init:s0",secdata);
 		security_release_secctx(secdata, seclen);
