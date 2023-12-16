34916aa73674639648b0a67e4d43ff8ffb96b3d8
HenryS <18201042@ittelkom-pwt.ac.id>
2023-12-16 10:05:28 +0000

    Revert "printk: block healthd messages in kernel log"
    This reverts commit 36b65afa562552a757815b4ccc2771a6687b1c51.

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -804,67 +804,7 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 {
 	/* Don't allow userspace to write to /dev/kmesg */
 	size_t len = iov_iter_count(from);
-	ssize_t ret = len;
-
-	if (!user || len > LOG_LINE_MAX)
-		return -EINVAL;
-
-	/* Ignore when user logging is disabled. */
-	if (devkmsg_log & DEVKMSG_LOG_MASK_OFF)
-		return len;
-
-	/* Ratelimit when not explicitly enabled. */
-	if (!(devkmsg_log & DEVKMSG_LOG_MASK_ON)) {
-		if (!___ratelimit(&user->rs, current->comm))
-			return ret;
-	}
-
-	buf = kmalloc(len+1, GFP_KERNEL);
-	if (buf == NULL)
-		return -ENOMEM;
-
-	buf[len] = '\0';
-	if (!copy_from_iter_full(buf, len, from)) {
-		kfree(buf);
-		return -EFAULT;
-	}
-
-	/*
-	 * Extract and skip the syslog prefix <[0-9]*>. Coming from userspace
-	 * the decimal value represents 32bit, the lower 3 bit are the log
-	 * level, the rest are the log facility.
-	 *
-	 * If no prefix or no userspace facility is specified, we
-	 * enforce LOG_USER, to be able to reliably distinguish
-	 * kernel-generated messages from userspace-injected ones.
-	 */
-	line = buf;
-	if (line[0] == '<') {
-		char *endp = NULL;
-		unsigned int u;
-
-		u = simple_strtoul(line + 1, &endp, 10);
-		if (endp && endp[0] == '>') {
-			level = LOG_LEVEL(u);
-			if (LOG_FACILITY(u) != 0)
-				facility = LOG_FACILITY(u);
-			endp++;
-			len -= endp - line;
-			line = endp;
-			/* QG-D */
-			if (line[0] == 'h') {
-				for (u = 0; u < 10; ++u) {
-					if (line[u] == 'd')
-						goto free;
-				}
-			}
-		}
-	}
-
-	printk_emit(facility, level, NULL, 0, "%s", line);
-free:
-	kfree(buf);
-	return ret;
+	return len;
 }
 
 
