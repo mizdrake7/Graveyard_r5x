5a07522a86b44883e943311f51dbad8e5a6332a2
Aner Torre <anernaiz@gmail.com>
2024-02-13 15:06:36 +0000

    boeffla_wl_blocker: Increase blacklist max length
    The previous 400 limit seemed to be not enough to hold all the Oppo stock defined wakelocks that look safe to block
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: Little-W <1405481963@qq.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -18,6 +18,6 @@
 
 #define LIST_WL_DEFAULT			"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_deauth_rec_wl;wlan_auth_req_wl;wlan_ap_assoc_lost_wl;wlan_pno_wl;wcnss_filter_lock;IPCRTR_lpass_rx;hal_bluetooth_lock;wlan_rx_wake;wlan_ctrl_wake;wlan_wake;qbt_wake_source;IPA_CLIENT_APPS_WAN_LOW_LAT_CONS;IPA_CLIENT_APPS_LAN_CONS;IPA_CLIENT_APPS_WAN_CONS;rmnet_ipa%d;rmnet_ctl;RMNET_SHS;4c90000.qcom,qup_uart;SensorsHAL_WAKEUP;scan_wake_lock;prox_lock;spi0.0;spi1.0;alarmtimer;event1;event2;eventpoll"
 
-#define LENGTH_LIST_WL			400
+#define LENGTH_LIST_WL			1024
 #define LENGTH_LIST_WL_DEFAULT		(sizeof(LIST_WL_DEFAULT))
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
