ae75622be05f131bc1811c335be82ee3a608be08
Shawn Lin <shawn.lin@rock-chips.com>
2024-02-19 03:01:16 +0000

    mmc: core: Add a timeout value for flushing cache In order to silent the warning below:
    mmc0: unspecified timeout for CMD6 - use generic
    
    Fixes: 4734c45 ("mmc: core: don't check card status when flushing cache")
    Signed-off-by: Shawn Lin <shawn.lin@rock-chips.com>
    Change-Id: I561fd8592c646a61d22b04e27a0fc0a6c9b01f4e

--- drivers/mmc/core/mmc_ops.c
+++ drivers/mmc/core/mmc_ops.c
@@ -1109,7 +1109,7 @@ int mmc_flush_cache(struct mmc_card *card)
 			(card->ext_csd.cache_ctrl & 1) &&
 			(!(card->quirks & MMC_QUIRK_CACHE_DISABLE))) {
 		err = mmc_switch(card, EXT_CSD_CMD_SET_NORMAL,
-				EXT_CSD_FLUSH_CACHE, 1, 0);
+				EXT_CSD_FLUSH_CACHE, 1, MMC_CACHE_FLUSH_TIMEOUT_MS);
 		if (err == -ETIMEDOUT) {
 			pr_err("%s: cache flush timeout\n",
 					mmc_hostname(card->host));
